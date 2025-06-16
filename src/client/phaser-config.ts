a1cc611faf55fbe32268cffe51d76ec90bd0604a
Paul Wooten <paul.wooten@hyland.com>
2025-06-16 16:05:39 -0400

    feat: tutorial continued

--- src/client/phaser-config.ts
+++ src/client/phaser-config.ts
@@ -2,9 +2,17 @@ import Phaser from 'phaser';
 type SceneCreateCallback = Phaser.Types.Scenes.SceneCreateCallback;
 type ScenePreloadCallback = Phaser.Types.Scenes.ScenePreloadCallback;
 type GameConfig = Phaser.Types.Core.GameConfig;
-
+type StaticGroup = Phaser.Physics.Arcade.StaticGroup;
+let platforms: StaticGroup | undefined;
 const create: SceneCreateCallback = function () {
     this.add.image(400, 300, 'sky');
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 };
 const preload: ScenePreloadCallback = function () {
     this.load.image('sky', 'assets/sky.png');
@@ -20,6 +28,13 @@ const phaserConfig: GameConfig = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x: 0, y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload,
         create,
