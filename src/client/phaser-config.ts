e567e76c4b7ba83a0abbb5f24e8d175009a7115b
Paul Wooten <paul.wooten@hyland.com>
2025-06-17 15:34:22 -0400

    feat: replace tutorial assets

--- src/client/phaser-config.ts
+++ src/client/phaser-config.ts
@@ -6,10 +6,8 @@ type GameConfig = Phaser.Types.Core.GameConfig;
 type StaticGroup = Phaser.Physics.Arcade.StaticGroup;
 type Sprite = Phaser.Physics.Arcade.Sprite;
 type CursorKeys = Phaser.Types.Input.Keyboard.CursorKeys;
-let platforms: StaticGroup | undefined;
-let bombs: Phaser.Physics.Arcade.Group | undefined;
-let player: Sprite | undefined;
-let stars: Phaser.Physics.Arcade.Group | undefined;
+
+let ship: Sprite | undefined;
 let score = 0;
 let scoreText: Phaser.GameObjects.Text | undefined;
 let cursorKeys: CursorKeys | undefined;
@@ -17,127 +15,55 @@ const create: SceneCreateCallback = function () {
     this.add.image(400, 300, 'sky');
     const textStyle: Phaser.Types.GameObjects.Text.TextStyle = { font: '32px Arial', color: '#ffffff' };
     scoreText = this.add.text(16, 16, 'Score: 0', textStyle);
-    platforms = this.physics.add.staticGroup();
 
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
-
-    stars = this.physics.add.group({
-        key: 'star',
-        repeat: 11,
-        setXY: { x: 12, y: 0, stepX: 70 }
-    });
-
-    stars.children.iterate(function (child) {
-        const gameObject = child as Sprite;
-        gameObject.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-        return true;
-    });
-    player = this.physics.add.sprite(100, 450, 'dude');
-
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-    const playerBody = player?.body as Phaser.Physics.Arcade.Body;
-    playerBody.setGravityY(300);
-
-    this.physics.add.collider(player, platforms);
-    this.physics.add.collider(stars, platforms);
-    this.physics.add.overlap(player, stars, function (player, star) {
-        const playerSprite = player as Sprite;
-        const sprite = star as Sprite;
-        sprite.disableBody(true, true);
-        score += 10;
-        scoreText?.setText('Score: ' + score);
-
-        if (stars?.countActive(true) === 0) {
-            for (const star of stars?.getChildren() || []) {
-                const sprite = star as Sprite;
-                sprite.enableBody(true, sprite.x, 0, true, true);
-            }
-
-            const x = (playerSprite.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-            const bomb = bombs?.create(x, 16, 'bomb');
-            bomb.setBounce(1);
-            bomb.setCollideWorldBounds(true);
-            bomb.setVelocity(Phaser.Math.Between(-200, 200), 2);
-
-        }
-    });
-
-    bombs = this.physics.add.group();
-
-    this.physics.add.collider(bombs, platforms);
-
-    this.physics.add.collider(player, bombs, (player, bomb) => {
-        this.physics.pause();
-        const playerSprite = player as Sprite;
-        playerSprite.setTint(0xff0000);
-
-        playerSprite.anims.play('turn');
-
-        // gameOver = true;
-        scoreText?.setText('Game Over! Final Score: ' + score);
-    }, () => { }, this);
+    ship = this.physics.add.sprite(400, 300, 'ship');
+    const shipBody = ship.body as Phaser.Physics.Arcade.Body;
+    shipBody.allowGravity = false;
+    ship.setCollideWorldBounds(true);
+    ship.setBounce(0.2);
 
     this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
+        key: 'ship-left',
+        frames: this.anims.generateFrameNumbers('ship', { start: 2, end: 2 }),
+        frameRate: 20,
         repeat: -1
     });
-
     this.anims.create({
-        key: 'turn',
-        frames: [{ key: 'dude', frame: 4 }],
-        frameRate: 20
+        key: 'ship-right',
+        frames: this.anims.generateFrameNumbers('ship', { start: 0, end: 0 }),
+        frameRate: 20,
+        repeat: -1
     });
-
     this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
+        key: 'ship',
+        frames: this.anims.generateFrameNumbers('ship', { start: 1, end: 1 }),
+        frameRate: 20,
         repeat: -1
     });
 
     cursorKeys = this?.input?.keyboard?.createCursorKeys();
 };
 const preload: ScenePreloadCallback = function () {
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
+    this.load.image('sky', 'assets/gradient-sky.png');
     this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude',
-        'assets/dude.png',
-        { frameWidth: 32, frameHeight: 48 }
-    );
+    this.load.spritesheet('ship', 'assets/ship.png',
+        { frameWidth: 32, frameHeight: 32 });
 };
 const update: SceneUpdateCallback = function () {
-    if (!player || !player.body || !platforms || !cursorKeys) {
+    if (!ship || !cursorKeys) {
         return;
     }
+
     if (cursorKeys.left.isDown) {
-        player.setVelocityX(-160);
-
-        player.anims.play('left', true);
+        ship.anims.play('ship-left', true);
+        return;
     }
-    else if (cursorKeys.right.isDown) {
-        player.setVelocityX(160);
-
-        player.anims.play('right', true);
-    }
-    else {
-        player.setVelocityX(0);
-
-        player.anims.play('turn');
+    if (cursorKeys.right.isDown) {
+        ship.anims.play('ship-right', true);
+        return;
     }
 
-    if (cursorKeys.up.isDown && player.body.touching.down) {
-        player.setVelocityY(-660);
-    }
+    ship.anims.play('ship');
 };
 const phaserConfig: GameConfig = {
     type: Phaser.AUTO,
@@ -146,7 +72,7 @@ const phaserConfig: GameConfig = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { x: 0, y: 300 },
+            gravity: { x: 0, y: 10 },
             debug: false
         }
     },
