c5a24fbd3d3a9f46b8ffe8db7d0e7d531f5eb64d
Paul Wooten <paul.wooten@hyland.com>
2025-06-17 08:55:48 -0400

    feat: tutorial pt 7

--- src/client/phaser-config.ts
+++ src/client/phaser-config.ts
@@ -1,9 +1,14 @@
 import Phaser from 'phaser';
 type SceneCreateCallback = Phaser.Types.Scenes.SceneCreateCallback;
 type ScenePreloadCallback = Phaser.Types.Scenes.ScenePreloadCallback;
+type SceneUpdateCallback = Phaser.Types.Scenes.SceneUpdateCallback;
 type GameConfig = Phaser.Types.Core.GameConfig;
 type StaticGroup = Phaser.Physics.Arcade.StaticGroup;
+type Sprite = Phaser.Physics.Arcade.Sprite;
+type CursorKeys = Phaser.Types.Input.Keyboard.CursorKeys;
 let platforms: StaticGroup | undefined;
+let player: Sprite | undefined;
+let cursorKeys: CursorKeys | undefined;
 const create: SceneCreateCallback = function () {
     this.add.image(400, 300, 'sky');
     platforms = this.physics.add.staticGroup();
@@ -13,6 +18,36 @@ const create: SceneCreateCallback = function () {
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    const playerBody = player?.body as Phaser.Physics.Arcade.Body;
+    playerBody.setGravityY(300);
+
+    this.physics.add.collider(player, platforms);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    cursorKeys = this?.input?.keyboard?.createCursorKeys();
 };
 const preload: ScenePreloadCallback = function () {
     this.load.image('sky', 'assets/sky.png');
@@ -23,6 +58,30 @@ const preload: ScenePreloadCallback = function () {
         'assets/dude.png',
         { frameWidth: 32, frameHeight: 48 }
     );
+};
+const update: SceneUpdateCallback = function () {
+    if (!player || !player.body || !platforms || !cursorKeys) {
+        return;
+    }
+    if (cursorKeys.left.isDown) {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursorKeys.right.isDown) {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursorKeys.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-660);
+    }
 };
 const phaserConfig: GameConfig = {
     type: Phaser.AUTO,
@@ -38,7 +97,7 @@ const phaserConfig: GameConfig = {
     scene: {
         preload,
         create,
-        update: () => { }
+        update
     }
 };
 
